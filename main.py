c2d562a547494a32ccda26bea6023daf81bc8ced
samajid80 <samajid80@yahoo.com>
2026-04-24 04:07:44 +0500

    Add post method in main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from datetime import datetime
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    category: str
 
 app = FastAPI(title="Clinic API", version="1.0.0")
 
@@ -22,4 +27,12 @@ def get_items():
             {"id": 2, "name": "Bandage", "category": "supplies"},
             {"id": 3, "name": "Syringe", "category": "equipment"},
         ]
+    }
+
+@app.post("/items")
+def create_item(item: Item):
+    return {
+        "message": f"Item '{item.name}' received.",
+        "data": item,
+        "id": 99
     }
